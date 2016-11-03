cc34f9dd4dffa5957756bbb18fca4b635a0c0777
AriellaYamada <ariella.y.b@hotmail.com>
2016-11-03 23:18:00 +0000

    Arrumando limpar instruções

--- js/jogo.js
+++ js/jogo.js
@@ -5,33 +5,32 @@ var exec_rows = '';
 function generate_instr(){
 	instructions = '';
 	clear_instr();
-	for (var i = 0; i < 20; i++) {
+	for (var i = 0; i < 10; i++) {
 		var instr = new Instr(Math.floor(Math.random() * 3));
 		instructions += '<tr><td>' + i + '</td><td>' + instr.getString() + '</td></tr>';
 	}
-	$('#instructions').html(instructions);
+	document.getElementById('instructions').innerHTML = instructions;
 	$("#instructions tr").dblclick(function(){
-	  $(this).addClass('selected').siblings().removeClass('selected');
-	  var instr = $(this).find('td:eq(1)').html();
+		$(this).addClass('selected').siblings().removeClass('selected');
+		var instr = $(this).find('td:eq(1)').html();
 		$(this).remove();
 		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
 		exec_count++;
-		$('#executions').html(exec_rows);
-	});
-	$("#executions tr").dblclick(function(){
-	  $(this).addClass('selected').siblings().removeClass('selected');
-		//var i = $(this).find('td:first').html();
-		var instr = $(this).find('td:eq(1)').html();
-		$(this).remove();
-		var temp = document.getElementById('instructions').innerHTML;
-		temp += '<tr><td>' + exec_rows + '</td><td>' + instr + '</td></tr>';
-		$('#instructions').html(temp);
+		document.getElementById('executions').innerHTML = exec_rows;
 	});
 }
 
 function clear_instr() {
-	$('#instructions').html(instructions);
-	document.getElementById('executions').innerHTML = '';
 	exec_count = 0;
 	exec_rows = '';
+	document.getElementById('instructions').innerHTML = instructions;
+	document.getElementById('executions').innerHTML = exec_rows;
+	$("#instructions tr").dblclick(function(){
+		$(this).addClass('selected').siblings().removeClass('selected');
+		var instr = $(this).find('td:eq(1)').html();
+		$(this).remove();
+		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
+		exec_count++;
+		document.getElementById('executions').innerHTML = exec_rows;
+	});
 }
