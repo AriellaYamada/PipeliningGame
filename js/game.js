e662316893941ee5fa1552f1393b046d3a656a20
AriellaYamada <ariella.y.b@hotmail.com>
2016-11-03 01:41:41 +0000

    Passando instruções para execução

--- js/game.js
+++
@@ -1,5 +0,0 @@
-$("#instructions tr").click(function(){
-  $(this).addClass('selected').siblings().removeClass('selected');
-  var instr = $(this).find('td:second').html();
-  console.log(instr);
-});
--- js/gerador.js
+++ js/gerador.js
@@ -10,6 +10,8 @@ const MAX_IMMEDIATE_VAL = 100;	// valor máximo que um valor imediato numa instr
 const LS_INSTR_NAMES = ["sw", "lw", "sb", "lb"];	// nomes de instruções load/store
 const AL_INSTR_NAMES = ["add", "sub", "addu", "subu", "mult", "multu", "div", "divu", "mul", "and", "or", "nor", "xor", "sll", "srl", "sra", "addi", "addiu", "andi", "ori", "xori"];						  // nomes de instruções ariméticas/lógicas
 
+var exec_count = 0;
+var exec_rows = '';
 // Retorna id de um registrador aleatório. Intervalo [0, N_REGISTERS-1].
 function random_register()
 {
@@ -105,8 +107,18 @@ function generate_instr(){
 	}
 	console.log(html);
 	$('#instructions').html(html);
+	$("#instructions tr").dblclick(function(){
+	  $(this).addClass('selected').siblings().removeClass('selected');
+	  var instr = $(this).find('td:eq(1)').html();
+		$(this).remove();
+		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
+		exec_count++;
+		$('#executions').html(exec_rows);
+	});
 }
 
 function clear_instr() {
 	document.getElementById('instructions').innerHTML = '';
+	exec_count = 0;
+	exec_rows = '';
 }
