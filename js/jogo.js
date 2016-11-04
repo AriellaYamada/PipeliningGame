b7354b1658b938c0eeac7807e84842373c8f5e73
AriellaYamada <ariella.y.b@hotmail.com>
2016-11-04 00:30:59 +0000

    Tentando fazer a verificacao

--- js/jogo.js
+++ js/jogo.js
@@ -1,17 +1,27 @@
+const NUM_INSTR = 10;
+
 var instructions;
 var exec_count = 0;
 var exec_rows = '';
 
+let vet_instr;
+vet_instr.length = NUM_INSTR;
+let vet_ordem;
+vet_ordem.length = NUM_INSTR;
+
 function gera_instrucoes(){
 	instructions = '';
 	limpa_instrucoes();
-	for (var i = 0; i < 10; i++) {
-		var instr = new Instr(Math.floor(Math.random() * 3));
+	for (var i = 0; i < NUM_INSTR; i++) {
+		let instr = new Instr(Math.floor(Math.random() * 3));
+		vet_instr[i] = instr;
 		instructions += '<tr><td>' + i + '</td><td>' + instr.getString() + '</td></tr>';
 	}
 	document.getElementById('instructions').innerHTML = instructions;
 	$("#instructions tr").dblclick(function(){
 		$(this).addClass('selected').siblings().removeClass('selected');
+		vet_ordem[exec_count] = $(this).find('td:first').html();
+		console.log(vet_ordem[exec_count]);
 		var instr = $(this).find('td:eq(1)').html();
 		$(this).remove();
 		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
@@ -35,7 +45,9 @@ function limpa_instrucoes() {
 	});
 }
 
-function ver_end() {
+/*function ver_end() {
 	var size = document.getElementById('executions').rows.length;
-	//if(size == 10)
-}
+	if(size == NUM_INSTR) {
+
+	}
+}*/
