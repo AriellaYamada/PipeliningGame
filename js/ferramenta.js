f1a5aadcefee09ef2a2880236f5cc11f9399594e
AriellaYamada <ariella.y.b@hotmail.com>
2016-11-04 14:54:12 +0000

    Adicionando noops manualmente

--- js/ferramenta.js
+++ js/ferramenta.js
@@ -5,7 +5,7 @@ var exec_rows = '';
 var exec_count = 0;
 
 var vet_instr = [];
-var vet_ordem = [];
+var v = [];
 
 function gera_instrucoes(){
 	instructions = '';
@@ -18,26 +18,37 @@ function gera_instrucoes(){
 	document.getElementById('instructions').innerHTML = instructions;
 	$("#instructions tr").dblclick(function(){
 		$(this).addClass('selected').siblings().removeClass('selected');
-		vet_ordem.push($(this).find('td:first').html());
+		var pos = $(this).find('td:first').html();
+		console.log(pos);
 		let instr = $(this).find('td:eq(1)').html();
 		$(this).remove();
+		v.push(vet_instr[pos]);
 		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
 		exec_count++;
 		document.getElementById('executions').innerHTML = exec_rows;
 	});
 }
 
+function adiciona_noop() {
+	console.log('teste');
+	let instr = new Instr(NOOP_INSTR);
+	v.push(instr);
+	exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr.getString() + '</td></tr>';
+	exec_count++;
+	document.getElementById('executions').innerHTML = exec_rows;
+}
+
 function limpa_instrucoes() {
 	exec_count = 0;
 	exec_rows = '';
-	vet_ordem.length = 0;
+	v.length = 0;
 	document.getElementById('instructions').innerHTML = instructions;
 	document.getElementById('executions').innerHTML = exec_rows;
 	$("#instructions tr").dblclick(function(){
 		$(this).addClass('selected').siblings().removeClass('selected');
-		vet_ordem.push($(this).find('td:first').html());
 		let instr = $(this).find('td:eq(1)').html();
 		$(this).remove();
+		v.push(vet_instr[pos]);
 		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
 		exec_count++;
 		document.getElementById('executions').innerHTML = exec_rows;
@@ -47,10 +58,6 @@ function limpa_instrucoes() {
 function ver_end() {
 	let size = document.getElementById('executions').rows.length;
 	if(size == NUM_INSTR) {
-		let v = [];
-		for(let i = 0; i < NUM_INSTR; i++) {
-			v.push(vet_instr[vet_ordem[i]]);
-		}
 		let dados_flag = verifica_dados(v);
 		let estrut_flag = verifica_estrutural(v);
 		console.log("Dados: " + dados_flag + "\nEstrutural: " + estrut_flag);
