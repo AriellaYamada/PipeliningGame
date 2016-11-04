6761647652043b7a592b7728f904e6b446810a4f
AriellaYamada <ariella.y.b@hotmail.com>
2016-11-04 16:09:44 +0000

    Verificando instruções completas

--- js/ferramenta.js
+++ js/ferramenta.js
@@ -3,6 +3,7 @@ const NUM_INSTR = 10;
 var instructions;
 var exec_rows = '';
 var exec_count = 0;
+var instr_count = 0;
 
 var vet_instr = [];
 var v = [];
@@ -11,7 +12,7 @@ function gera_instrucoes(){
 	instructions = '';
 	limpa_instrucoes();
 	for (let i = 0; i < NUM_INSTR; i++) {
-		let instr = new Instr(Math.floor(Math.random() * 3));
+		let instr = new Instr(Math.floor(Math.random() * 2));
 		vet_instr.push(instr);
 		instructions += '<tr><td>' + i + '</td><td>' + instr.getString() + '</td></tr>';
 	}
@@ -20,10 +21,10 @@ function gera_instrucoes(){
 		$(this).addClass('selected').siblings().removeClass('selected');
 		var pos = $(this).find('td:first').html();
 		let instr = $(this).find('td:eq(1)').html();
-		//$(this).prop('disabled', true);
 		v.push(vet_instr[pos]);
 		exec_rows += '<tr><td>' + exec_count + '</td><td>' + instr + '</td></tr>';
 		exec_count++;
+		instr_count++;
 		document.getElementById('executions').innerHTML = exec_rows;
 		document.getElementById('instructions').rows[pos].style.pointerEvents = 'none';
 		document.getElementById('instructions').rows[pos].style.backgroundColor = '#f2dede';
@@ -41,6 +42,7 @@ function adiciona_noop() {
 function limpa_instrucoes() {
 	exec_count = 0;
 	exec_rows = '';
+	instr_count = 0;
 	v.length = 0;
 	document.getElementById('instructions').innerHTML = instructions;
 	document.getElementById('executions').innerHTML = exec_rows;
@@ -56,8 +58,12 @@ function limpa_instrucoes() {
 }
 
 function ver_end() {
-	let size = document.getElementById('executions').rows.length;
-		let dados_flag = verifica_dados(v);
-		let estrut_flag = verifica_estrutural(v);
-		console.log("Dados: " + dados_flag + "\nEstrutural: " + estrut_flag);
+	if(instr_count >= NUM_INSTR){
+		let size = document.getElementById('executions').rows.length;
+			let dados_flag = verifica_dados(v);
+			let estrut_flag = verifica_estrutural(v);
+			console.log("Dados: " + dados_flag + "\nEstrutural: " + estrut_flag);
+	} else {
+		console.log("Não finalizado!");
+	}
 }
